67553b230b59dee5f112d906937e6c6b5a6c5d9a
jdrydn <james@jdrydn.com>
2015-12-17 11:30:45 +0000

    Unit tests spot all the problems!

--- cron.js
+++ cron.js
@@ -4,27 +4,23 @@ var later = require('later');
 var EVENTS = [ 'start', 'stop' ];
 
 function Cron() {
-  this.args = [];
-  this.flags = [];
+  this._alias = {};
 
-  this.aliases = {};
-
-  this.crons = {};
-  this.events = {};
+  this._crons = {};
+  this._events = {};
 }
 
 Cron.prototype.aliases = function (raw) {
-  for (var time in raw) if (raw.hasOwnProperty(time)) {
-    (Array.isArray(raw[time]) ? raw[time] : [ raw[time] ]).forEach(function (time) {
-      this.aliases[alias] = time;
-    });
+  for (var time in raw) /* istanbul ignore else */ if (raw.hasOwnProperty(time)) {
+    if (!Array.isArray(raw[time])) raw[time] = [ raw[time] ];
+    for (var i = 0; i < raw[time].length; i++) this._alias[raw[time][i]] = time;
   }
   return this;
 };
 
 Cron.prototype.on = function (cronTime, cronFn) {
-  var target = EVENTS.indexOf(cronTime.toString().toLowerCase()) >= 0 ? 'events' : 'crons';
-  if (target === 'crons' && this.aliases.hasOwnProperty(cronTime)) cronTime = this.aliases[cronTime];
+  var target = EVENTS.indexOf(cronTime.toString().toLowerCase()) >= 0 ? '_events' : '_crons';
+  if (target === '_crons' && this._alias.hasOwnProperty(cronTime)) cronTime = this._alias[cronTime];
 
   this[target][cronTime] = this[target][cronTime] || [];
   if (Array.isArray(cronFn)) this[target][cronTime] = this[target][cronTime].concat(cronFn);
@@ -35,6 +31,7 @@ Cron.prototype.on = function (cronTime, cronFn) {
 
 var run = false;
 Cron.prototype.run = function (callback) {
+  /* istanbul ignore next */
   if (!callback) callback = function (err) {
     if (err) console.error(err.toString());
     process.exit(err ? 1 : 0);
@@ -47,21 +44,19 @@ Cron.prototype.run = function (callback) {
   date.setSeconds(0); // Date has seconds, and we don't want seconds (yet!)
 
   var fns = [];
-  Object.keys(this.crons).forEach(function (time) {
-    if (later.schedule(later.parse.cron(time)).isValid(date)) {
-      fns = fns.concat(this.crons[time]);
-    }
+  Object.keys(this._crons).forEach(function (time) {
+    if (later.schedule(later.parse.cron(time)).isValid(date)) fns = fns.concat(this._crons[time]);
   }.bind(this));
 
   async.series([].concat(
-    Array.isArray(this.events.start) ? this.events.start : [],
+    Array.isArray(this._events.start) ? this._events.start : [],
     async.parallel.bind(async.parallel, fns),
-    Array.isArray(this.events.stop) ? this.events.stop : []
+    Array.isArray(this._events.stop) ? this._events.stop : []
   ), callback);
 };
 
 Cron.prototype.stop = function (callback) {
-  async.series(Array.isArray(this.events.stop) ? this.events.stop : [], callback);
+  async.series(Array.isArray(this._events.stop) ? this._events.stop : [], callback);
 };
 
 var cronfile = new Cron();
