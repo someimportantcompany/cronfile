2c191a10f6b62d07b9e61472f188d4b5be0185e4
jdrydn <james@jdrydn.com>
2016-11-29 19:13:16 +0000

    Removing chill-logger & colors dependencies

--- example/cronfile.js
+++ example/cronfile.js
@@ -1,41 +1,27 @@
-var cron = require('../lib/cron'); // cronfile
-var logger = require('chill-logger')({ name: 'cron' });
-var slack = require('../plugins/slack'); // cronfile/plugins/slack
+var cron = require('../index'); // cronfile
+// var slack = require('../plugins/slack'); // cronfile/plugins/slack
 
 cron.once('started', function (wait_time) {
-  logger.info('started', { waiting: wait_time });
+  console.log('cronfile has started, waiting %s', wait_time);
 });
 
 cron.on('tick', function (date, matches) {
-  logger.info('tick', {
-    date: date.toString(),
-    matches: matches
-  });
+  console.log('tick %s %d matches', date, matches);
 });
 
 cron.on('error', function (err, date) {
   if (date) err.message = 'An error occurred executing a cron at ' + date.toString() + ': ' + err.message;
-  logger.error(err);
+  console.error(err);
 });
 
-cron.on('results', slack({
-  webhook_url: 'https://www.slack.com/.../.../.../'
-}));
+// cron.on('results', slack({
+//   webhook_url: 'https://www.slack.com/.../.../.../'
+// }));
 
 cron.on('every_minute', function () {
-  logger.debug('Hello, world!');
+  console.log('Every minute');
 });
 
-cron.on('every_two_minutes', [
-  function () {
-    logger.debug('Hello, world #1!');
-  },
-  function () {
-    logger.debug('Hello, world #2!');
-  }
-]);
-
-process.on('uncaughtException', function (err) {
-  logger.error('fatal', err);
-  process.exit(1);
+cron.on('every_two_minutes', function () {
+  console.log('Every two minutes');
 });
